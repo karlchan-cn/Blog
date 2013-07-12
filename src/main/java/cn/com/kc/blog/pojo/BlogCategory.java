f04729eee57e3c96d8b6652a30766472296c9bbd
kchen1 <kchen1@zhu12596.prod.quest.corp>
2013-07-12 17:55:41 +0800

    Fix table connection problem

--- src/main/java/cn/com/kc/blog/pojo/BlogCategory.java
+++ src/main/java/cn/com/kc/blog/pojo/BlogCategory.java
@@ -20,61 +20,62 @@ import javax.persistence.Table;
 @Table(name = "BLOG_CATEGORY")
 public class BlogCategory implements Serializable {
 
-	/**
+/**
 	 * 
 	 */
-	private static final long serialVersionUID = -2243315009224421667L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String name;
-	@ManyToOne(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, targetEntity = BlogCategory.class)
-	@JoinColumn(name = "PARENT_ID", nullable = true)
-	private BlogCategory parent;
-	@OneToMany(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, mappedBy = "parent")
-	private List<BlogCategory> children;
-	@ManyToMany(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, targetEntity = BlogEntity.class)
-	@JoinTable(name = "BLOG_ENTITY_CATE")
-	private List<BlogEntity> entities;
+private static final long serialVersionUID = -2243315009224421667L;
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+private String name;
+@ManyToOne(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, targetEntity = BlogCategory.class)
+@JoinColumn(name = "PARENT_ID", nullable = true)
+private BlogCategory parent;
+@OneToMany(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, mappedBy = "parent")
+private List<BlogCategory> children;
+@ManyToMany(cascade = { CascadeType.PERSIST }, fetch = FetchType.LAZY, targetEntity = BlogEntity.class)
+@JoinTable(name = "BLOG_ENTITY_CATE", joinColumns = { @JoinColumn(name = "ENTITY_ID") },
+				inverseJoinColumns = { @JoinColumn(name = "CATE_ID") })
+private List<BlogEntity> entities;
 
-	public List<BlogEntity> getEntities() {
-		return entities;
-	}
+public List<BlogEntity> getEntities() {
+	return entities;
+}
 
-	public void setEntities(List<BlogEntity> entities) {
-		this.entities = entities;
-	}
+public void setEntities(List<BlogEntity> entities) {
+	this.entities = entities;
+}
 
-	public Long getId() {
-		return id;
-	}
+public Long getId() {
+	return id;
+}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+public void setId(Long id) {
+	this.id = id;
+}
 
-	public String getName() {
-		return name;
-	}
+public String getName() {
+	return name;
+}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+public void setName(String name) {
+	this.name = name;
+}
 
-	public BlogCategory getParent() {
-		return parent;
-	}
+public BlogCategory getParent() {
+	return parent;
+}
 
-	public void setParent(BlogCategory parent) {
-		this.parent = parent;
-	}
+public void setParent(BlogCategory parent) {
+	this.parent = parent;
+}
 
-	public List<BlogCategory> getChildren() {
-		return children;
-	}
+public List<BlogCategory> getChildren() {
+	return children;
+}
 
-	public void setChildren(List<BlogCategory> children) {
-		this.children = children;
-	}
+public void setChildren(List<BlogCategory> children) {
+	this.children = children;
+}
 
 }
